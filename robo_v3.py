9466721fa808461e0fbd751754dcbd753e21485d
Pedro Henrique Correa Mota da Silva <pedrohcms2018@gmail.com>
2019-10-18 15:43:07 -0300

    Mundanças nos códigos

--- robo_v3.py
+++ robo_v3.py
@@ -5,9 +5,9 @@ import pandas as pd
 import xlrd
 import os
 from datetime import date
+from dateutil.parser import parse
 import time
 
-init_time = time.time()
 op = 1
 
 while op == 1:
@@ -24,16 +24,16 @@ while op == 1:
         else:
             print('Arquivo recebido com sucesso!')
 
-            sheet_name = input('Digite o nome da planilha: ')
-
             resposta = input('A panilha foi ordenada de maneira crescente nos campos de interface e empresa, respectivamente Pressione S - SIM |N - NÃO?: ')
 
             if resposta == 'N':
                 print('Ordene as colunas primeiro e depois utilize o algoritmo')
                 exit()
+            
+            sheet_name = input('Digite o nome da planilha: ')
 
             try:
-                document = pd.read_excel(path, sheet_name=sheet_name)    
+                document = pd.read_excel(path, sheet_name=sheet_name).astype(str)
             except:
                 print('A panilha desejada não foi encontrada!')
                 continue
@@ -46,44 +46,59 @@ while op == 1:
             #Criando o arquivo txt caso ele não exista
             txt = open('./resultados/'+sheet_name+'_'+str(date.today().year)+'.txt', 'w')
 
-            init_time = time.time()
-
             #Iniciando os DataFrames
-            interface = pd.DataFrame(document, columns= ['Interface'])
-            empr = pd.DataFrame(document, columns= ['Empr'])
-            cl = pd.DataFrame(document, columns= ['CL'])
-            conta = pd.DataFrame(document, columns= ['Conta'])
-            montante = pd.DataFrame(document, columns= ['Valor do Montante'])
-            pep = pd.DataFrame(document, columns= ['Elemento PEP'])
-            chave_ref = pd.DataFrame(document, columns= ['Chv.ref.1'])
-            data_doc = pd.DataFrame(document, columns= ['Data do Doc'])
-            contrato = pd.DataFrame(document, columns= ['Contrato'])
-            data_lancamento = pd.DataFrame(document, columns= ['Data Lançamento'])
-            historico = pd.DataFrame(document, columns= ['Denominação'])
+            interface = pd.DataFrame(document, columns = ['Interface'])
+            empr = pd.DataFrame(document, columns = ['Empr'])
+            cl = pd.DataFrame(document, columns = ['CL'])
+            conta = pd.DataFrame(document, columns = ['Conta'])
+            montante = pd.DataFrame(document, columns = ['Valor do Montante'])
+            pep = pd.DataFrame(document, columns = ['Elemento PEP'])
+            chave_ref = pd.DataFrame(document, columns = ['Chv.ref.1'])
+            data_doc = pd.DataFrame(document, columns = ['Data do Doc'])
+            contrato = pd.DataFrame(document, columns = ['Contrato'])
+            data_lancamento = pd.DataFrame(document, columns = ['Data Lançamento'])
+            historico = pd.DataFrame(document, columns = ['Denominação'])
+
+            init_time = time.perf_counter()
 
             for linha in range(document.shape[0]):
                 string_linha = '&SdtTexto.Add(\''
                 
                 #empresa
-                string = str(empr.loc[linha][0])
+                string = empr.iloc[linha][0]
 
-                if len(string) != 5:
+                if len(string) > 5 or len(string) < 3:
+                    print('O valor do campo Empresa é inválido')
+                    break
+                else:
                     string = string.zfill(5)
 
                 string_linha += string
                 
-                #Débito ou crédito
-                string = str(cl.loc[linha][0])
+                #Débito ou crédito(CL)
+                string = cl.iloc[linha][0]
 
-                string_linha += string
+                if (string == 'C' or 'D') and len(string) == 1:
+                    string_linha += string
+                else:
+                    print('O valor do campo CL é invalido')
+                    break
 
                 #Conta contabil
-                string = str(conta.loc[linha][0])
+                string = conta.iloc[linha][0]
+
+                if len(string) != 10:
+                    print('O valor do campo Conta contábil é inválido')
+                    break
 
                 string_linha += string
 
                 #Montante
-                string = str(montante.loc[linha][0])
+                string = montante.iloc[linha][0]
+
+                if len(string) > 15:
+                    print('O valor do campo montante é inválido')
+                    break
 
                 if '.' in string:
                     string = string.split('.')
@@ -97,45 +112,68 @@ while op == 1:
                 string_linha += string
                 
                 #PEP
-                string = str(pep.loc[linha][0])
+                string = pep.iloc[linha][0]
                 
-                string = string + ' ' * (23 - len(string)) 
+                if len(string) == 15:
+                    string = string + ' ' * (23 - len(string)) 
 
-                string_linha += string
+                    string_linha += string
+                else:
+                    print('O valor do campo Elemento PEP é inválido')
+                    break
                 
                 #Chav. Ref. 1
-                string = str(chave_ref.loc[linha][0])
+                string = chave_ref.iloc[linha][0]
                 
                 if string == 'nan':
                     string = ' ' * (12) 
+                elif len(string) == 12:
+                    pass
                 else:
-                    string = string + ' ' * (12 - len(string))
+                    print('O valor do campo chave referência é inválido')
+                    break
 
                 string_linha += string
 
                 #Data Documento
-                string = data_doc.loc[linha][0]
+                string = data_doc.iloc[linha][0]
 
+                if len(string) != 10:
+                    print('O valor do campo data é inválido')
+                    break
+            
+                string = parse(string)
                 string = format(string, "%Y%m%d")
 
                 string_linha += string
 
                 #Contrato
-                string = str(contrato.loc[linha][0])
-
+                string = contrato.iloc[linha][0]
+                
+                if len(string) < 5 or len(string) > 6:
+                    print('O valor do campo contrato é inválido')
+                    break
+                elif len(string) == 5:
+                    print('AVISO! O valor do campo contrato na linha: '+ linha + ' tem 5 números')
+                
                 string = string.zfill(6)
 
                 string_linha += string
 
                 #Data do lançamento
-                string = data_lancamento.loc[linha][0]
+                string = data_lancamento.iloc[linha][0]
 
+                if len(string) != 10:
+                    print('O valor do campo data é inválido')
+                    break
+
+                string = parse(string)
                 string = format(string, "%d/%m/%Y")
 
                 string_linha += string
                 
                 #Histórico(Denominação)
-                string = str(historico.loc[linha][0])
+                string = historico.iloc[linha][0]
 
                 if len(string) >= 50:
                     string = string[0:50]
@@ -144,7 +182,7 @@ while op == 1:
 
                 string_linha += string
 
-                #Colocando o ')
+                #Coilocando o ')
                 string_linha += '\')'
 
                 #Hora de salvar no arquivo
@@ -152,8 +190,8 @@ while op == 1:
                 txt.write('\n')
 
                 if linha < document.shape[0]-1: 
-                    interface_atual = str(interface.loc[linha][0])
-                    interface_prox = str(interface.loc[linha+1][0])
+                    interface_atual = str(interface.iloc[linha][0])
+                    interface_prox = str(interface.iloc[linha+1][0])
 
                     if interface_atual != interface_prox:
                         txt.write('Do \'Processar\'')
@@ -161,15 +199,16 @@ while op == 1:
                 
                 print('Linha: '+str(linha))
 
+            end_time = time.perf_counter()
+
             txt.write('Do \'Processar\'')
 
             txt.close()
 
-            end_time = time.time()
-
             print('O tempo de execução foi '+ str(end_time - init_time) + ' segundos')
 
     elif op == 0:
+        print('Saindo')
         break
     else:
         print('Opção inválida')
